c0e62b8a778fd871312da9c6f6ce962946351f18
alexandra david <alexdav13@gmail.com>
2024-08-09 12:01:11 +0200

    ex2

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -6,22 +6,25 @@ class BookmarksController < ApplicationController
 
   def create
     @list = List.find(params[:list_id])
-    @bookmark = @list.bookmarks.new(bookmark_params)
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
     if @bookmark.save
       redirect_to list_path(@list), notice: 'Bookmark was successfully created.'
     else
-      @bookmarks = @list.bookmarks.includes(:movie) # Assurez-vous de définir @bookmarks si le rendu échoue
-      render 'new'
+      render :new, status: :unprocessable_entity
     end
   end
 
-  # def destroy
-  #   @bookmark = Bookmark.find(params[:id])
-  #   @bookmark.destroy
-  #   redirect_to list_path(@bookmark.list), notice: 'Bookmark was successfully destroyed.'
-  # end
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), notice: 'Bookmark was successfully destroyed.'
+  end
 
   private
+
+  # def set_list
+  # end
 
   def bookmark_params
     params.require(:bookmark).permit(:comment, :movie_id)
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,8 @@
 Rails.application.routes.draw do
-  get 'movies/index'
-  get 'movies/show'
-  get 'bookmarks/create'
-  get 'bookmarks/destroy'
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/new'
-  get 'lists/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
   end
-  resources :movies, only: [:index, :show]
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :bookmarks, only: [:destroy]
   root to: 'lists#index'
 end
